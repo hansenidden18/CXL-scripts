b3ab668ab4dbbbf4a012ab42de6610baa0474d68
Hansen Idden <62301187+hansenidden18@users.noreply.github.com>
2024-03-11 04:14:13 +0700

    Update text_to_image.sh

--- text_to_image.sh
+++ text_to_image.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
-export ROOT=$PWD/inference
-export SD_FOLDER=$PWD/inference/text_to_image
-export LOADGEN_FOLDER=$PWD/inference/loadgen
-export MODEL_PATH=$PWD/inference/text_to_image/model/
+export ROOT=$PWD
+export SD_FOLDER=$PWD/text_to_image
+export LOADGEN_FOLDER=$PWD/loadgen
+export MODEL_PATH=$PWD/text_to_image/model/
 
 cd $SD_FOLDER
 
@@ -12,10 +11,12 @@ cp $ROOT/data/text_to_image-mlperf.conf $SD_FOLDER/mlperf.conf
 
 pip install -r requirements.txt
 pip install pandas
+pip install pybind11
 
 cd $LOADGEN_FOLDER
 CFLAGS="-std=c++14" python setup.py install
 
+cd $SD_FOLDER
 
 mkdir -p model
 
@@ -23,16 +24,16 @@ cd $MODEL_PATH
 
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 
-rclone config create mlc-inference s3 provider=Cloudflare \\
-	access_key_id=f65ba5eef400db161ea49967de89f47b \\
-	secret_access_key=fbea333914c292b854f14d3fe232bad6c5407bf0ab1bebf78833c2b359bdfd2b \\
-	endpoint=https://c2686074cb2caf5cbaf6d134bdba8b47.r2.cloudflarestorage.com
+rclone config create mlc-inference s3 provider=Cloudflare access_key_id=f65ba5eef400db161ea49967de89f47b secret_access_key=fbea333914c292b854f14d3fe232bad6c5407bf0ab1bebf78833c2b359bdfd2b endpoint=https://c2686074cb2caf5cbaf6d134bdba8b47.r2.cloudflarestorage.com
 
 rclone copy mlc-inference:mlcommons-inference-wg-public/stable_diffusion_fp32 ./stable_diffusion_fp32 -P
 
+mv stable_diffusion_fp32/* .
+rm -rf stable_diffusion_fp32
+
 cd $SD_FOLDER/tools
 
-./download-coco-2014.sh -n 20 -m 50
+./download-coco-2014.sh -n 20 -m 10
 
 cd $SD_FOLDER
 
